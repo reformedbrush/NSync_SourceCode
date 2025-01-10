e960c98840823b995a2faff3a216c1809cc112c8
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-01-10 13:17:37 +0530

    Admin Dasboard Sidebar

---
+++ Project/nsync_admin/lib/components/sidebar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SideBar extends StatefulWidget {
+  const SideBar({super.key});
+
+  @override
+  State<SideBar> createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(padding: EdgeInsets.zero, children: [
+        DrawerHeader(
+          child: Text(
+            "Dashboard Menu",
+            style: TextStyle(color: Colors.white),
+          ),
+          decoration: BoxDecoration(color: Colors.lightBlue),
+        ),
+        ListTile(
+          leading: Icon(Icons.supervised_user_circle),
+          title: Text("Profile"),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.volunteer_activism_rounded),
+          title: Text("Manage Events"),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.volunteer_activism_rounded),
+          title: Text("Manage Faculty"),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.volunteer_activism_rounded),
+          title: Text("Manage Students"),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.volunteer_activism_rounded),
+          title: Text("Manage Clubs"),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.build),
+          title: Text("Settings"),
+          onTap: () {},
+        ),
+        SizedBox(
+          height: 320,
+        ),
+        ListTile(
+          leading: Icon(Icons.logout_outlined),
+          title: Text("Logout"),
+          onTap: () {},
+        )
+      ]),
+    );
+  }
+}
--- Project/nsync_admin/lib/screen/admin_dash.dart
+++ Project/nsync_admin/lib/screen/admin_dash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nsync_admin/components/sidebar.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
@@ -11,32 +12,12 @@ class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 30),
-                              child: Image.asset(
-                                '../Assets/logo.png',
-                                width: 120,
-                                height: 120,
-                              )),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ))
-        ],
+      drawer: SideBar(),
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Center(
+        child: Text("Main Content Area"),
       ),
     );
   }
