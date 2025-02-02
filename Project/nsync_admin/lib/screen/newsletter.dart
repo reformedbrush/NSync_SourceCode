4631bc6903e1e0f296fce9902b1ab0953874a58f
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-02-02 16:44:24 +0530

    Newsletter Insertion

--- Project/nsync_admin/lib/screen/newsletter.dart
+++ Project/nsync_admin/lib/screen/newsletter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nsync_admin/components/insert_form.dart';
+import 'package:nsync_admin/main.dart';
 
 class NewsLetterScreen extends StatefulWidget {
   const NewsLetterScreen({super.key});
@@ -11,7 +13,42 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
     with SingleTickerProviderStateMixin {
   bool _isFormVisible = false;
 
+  //controller
+
+  final TextEditingController _newsTitleController = TextEditingController();
+  final TextEditingController _newsContentController = TextEditingController();
+  final TextEditingController _newsAuthorController = TextEditingController();
+
   final Duration _animationDuration = const Duration(milliseconds: 300);
+
+  // insertion
+
+  Future<void> insertNews() async {
+    try {
+      String Title = _newsTitleController.text;
+      String Author = _newsAuthorController.text;
+      String Content = _newsContentController.text;
+
+      await supabase.from('tbl_newsletter').insert({
+        'news_title': Title,
+        'news_author': Author,
+        'news_content': Content,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(
+          "Faculty Data Inserted Sucessfully",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+      ));
+      _newsAuthorController.clear();
+      _newsContentController.clear();
+      _newsTitleController.clear();
+    } catch (e) {
+      print("ERROR INSERTING DATA: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -49,13 +86,76 @@ class _NewsLetterScreenState extends State<NewsLetterScreen>
             ],
           ),
           AnimatedSize(
-              duration: _animationDuration,
-              curve: Curves.easeInOut,
-              child: _isFormVisible
-                  ? Form(
-                      child: Container(),
-                    )
-                  : Container())
+            duration: _animationDuration,
+            curve: Curves.easeInOut,
+            child: _isFormVisible
+                ? Form(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextFieldStyle(
+                                    label: "Title",
+                                    inputController: _newsTitleController),
+                              )),
+                              Expanded(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextFieldStyle(
+                                    label: "Author",
+                                    inputController: _newsAuthorController),
+                              ))
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextFieldStyle(
+                                    label: "Content",
+                                    inputController: _newsContentController),
+                              ))
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue,
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 22, horizontal: 70),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5))),
+                                  onPressed: () {
+                                    insertNews();
+                                  },
+                                  child: Text(
+                                    "Insert",
+                                    style: TextStyle(color: Colors.white),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                : Container(),
+          ),
+          Container(
+            child: Center(
+              child: Text(
+                "Published Newsletters",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+          )
         ],
       ),
     );
