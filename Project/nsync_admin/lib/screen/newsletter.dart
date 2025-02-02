d23c4b50cb4ec17549c0b201f74ad71f13c08b41
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-02-02 10:28:05 +0530

    Newsletter pg design

--- Project/nsync_admin/lib/screen/newsletter.dart
+++ Project/nsync_admin/lib/screen/newsletter.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
 
-class NewsLetterScreen extends StatelessWidget {
+class NewsLetterScreen extends StatefulWidget {
   const NewsLetterScreen({super.key});
 
   @override
+  State<NewsLetterScreen> createState() => _NewsLetterScreenState();
+}
+
+class _NewsLetterScreenState extends State<NewsLetterScreen>
+    with SingleTickerProviderStateMixin {
+  bool _isFormVisible = false;
+
+  final Duration _animationDuration = const Duration(milliseconds: 300);
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("NewsLetter Page"),
+    return Padding(
+      padding: EdgeInsets.all(18),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Post Newsletter"),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF161616),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 25, vertical: 18)),
+                  onPressed: () {
+                    setState(() {
+                      _isFormVisible = !_isFormVisible;
+                    });
+                  },
+                  label: Text(
+                    _isFormVisible ? "Cancel" : "New Newsletter",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  icon: Icon(
+                    _isFormVisible ? Icons.cancel : Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+          AnimatedSize(
+              duration: _animationDuration,
+              curve: Curves.easeInOut,
+              child: _isFormVisible
+                  ? Form(
+                      child: Container(),
+                    )
+                  : Container())
+        ],
+      ),
     );
   }
 }
