dc9a8b90e026493f816f90a558926f8ea23c6247
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-02-12 11:23:08 +0530

    feb 12

---
+++ Project/nsync_student/lib/screen/student_club.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:nsync_student/components/appbar.dart';
+
+class StuClub extends StatelessWidget {
+  const StuClub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text("Club Events Page")),
+      ),
+    );
+  }
+}
