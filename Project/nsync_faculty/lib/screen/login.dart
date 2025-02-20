94e3075fc09fd396277ffe22c76f4f6940e8b850
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-02-20 20:15:48 +0530

    Fac login redo

--- Project/nsync_faculty/lib/screen/login.dart
+++ Project/nsync_faculty/lib/screen/login.dart
@@ -1,6 +1,7 @@
 import 'package:cherry_toast/resources/arrays.dart';
 import 'package:flutter/material.dart';
 import 'package:nsync_faculty/components/clrsafearea.dart';
+import 'package:nsync_faculty/components/formvalidation.dart';
 import 'package:nsync_faculty/main.dart';
 import 'package:nsync_faculty/screen/facultyhome.dart';
 import 'package:cherry_toast/cherry_toast.dart';
@@ -13,157 +14,163 @@ class Login1 extends StatefulWidget {
 }
 
 class _Login1State extends State<Login1> {
-  // signin
+  final formKey = GlobalKey<FormState>();
+
+  // Controllers
+  final TextEditingController _facPasswordController = TextEditingController();
+  final TextEditingController _facEmailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _facPasswordController.dispose();
+    _facEmailController.dispose();
+    super.dispose();
+  }
+
+  // Sign-in function
   Future<void> signin() async {
     try {
       final auth = await supabase.auth.signInWithPassword(
-          password: _facPasswordController.text,
-          email: _facEmailController.text);
+        password: _facPasswordController.text,
+        email: _facEmailController.text,
+      );
+
       String uid = auth.user!.id;
       final res = await supabase
           .from("tbl_faculty")
           .select()
           .eq("faculty_id", uid)
-          .limit(1);
-      if (res.length > 0) {
+          .maybeSingle();
+
+      if (res != null) {
         Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Facultyhome(),
-            ));
+          context,
+          MaterialPageRoute(builder: (context) => const Facultyhome()),
+        );
       } else {
-        print("Invalid User");
+        CherryToast.error(
+          description: const Text(
+            "Invalid User",
+            style: TextStyle(color: Colors.black),
+          ),
+          animationType: AnimationType.fromRight,
+          animationDuration: const Duration(milliseconds: 1000),
+          autoDismiss: true,
+        ).show(context);
       }
     } catch (e) {
-      print("ERROR LOGIN: $e");
       CherryToast.error(
-              description: Text("No user found for that email.",
-                  style: TextStyle(color: Colors.black)),
-              animationType: AnimationType.fromRight,
-              animationDuration: Duration(milliseconds: 1000),
-              autoDismiss: true)
-          .show(context);
-      print('No user found for that email.');
+        description: Text(
+          "$e",
+          style: const TextStyle(color: Colors.black),
+        ),
+        animationType: AnimationType.fromRight,
+        animationDuration: const Duration(milliseconds: 1000),
+        autoDismiss: true,
+      ).show(context);
     }
   }
-
-  TextEditingController _facPasswordController = TextEditingController();
-
-  TextEditingController _facEmailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ColoredSafeArea(
-            child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/Group 6.png")),
-              color: const Color.fromARGB(255, 12, 12, 11)),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(8.0),
           child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 180,
-                ),
-                Container(
-                  width: 340,
-                  height: 500,
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Image.asset('./assets/logo.png'),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Center(
-                        child: Text(
-                          "Sign in to your",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          "Account",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 35,
-                      ),
-                      TextField(
-                        controller: _facEmailController,
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          filled: true,
-                          hintText: 'E-mail ID',
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 1,
-                      ),
-                      TextField(
-                        controller: _facPasswordController,
-                        decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: 'Password',
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.white,
-                            ))),
-                      ),
-                      SizedBox(
-                        height: 24,
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            "Forget Password?",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF1D61E7),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 145, vertical: 18)),
-                          onPressed: () {
-                            /*  Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => FacultyHome(),
-                                )); */
-                            signin();
-                          },
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(color: Colors.white),
-                          ))
-                    ],
+            child: Container(
+              height: 650,
+              width: 900,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.grey)),
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(
+                        "../assets/Brazuca.png"), // Fixed asset path
                   ),
-                ),
-              ],
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 50.0, right: 40),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            TextFormField(
+                              validator: (value) =>
+                                  FormValidation.validateEmail(value),
+                              controller: _facEmailController,
+                              decoration: const InputDecoration(
+                                hintText: "Login",
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            TextFormField(
+                              validator: (value) =>
+                                  FormValidation.validatePassword(value),
+                              controller: _facPasswordController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                hintText: "Password",
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xFF161616),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 150, vertical: 25)),
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  signin();
+                                }
+                              },
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        )),
+        ),
       ),
     );
   }
