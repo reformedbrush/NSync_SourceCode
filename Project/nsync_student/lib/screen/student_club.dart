da3ee5f82a23619cb423dc6bcd05f102e21d5842
Akshai R Sangeeth <akshayrs865@gmail.com>
2025-03-11 23:42:30 +0530

    student app club list view builder

--- Project/nsync_student/lib/screen/student_club.dart
+++ Project/nsync_student/lib/screen/student_club.dart
@@ -1,37 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:nsync_student/main.dart';
 
-class StuClub extends StatelessWidget {
+class StuClub extends StatefulWidget {
   const StuClub({super.key});
+
+  @override
+  State<StuClub> createState() => _StuClubState();
+}
+
+class _StuClubState extends State<StuClub> {
+  List<Map<String, dynamic>> Clublist = [];
+
+  //select
+
+  Future<void> fecthClub() async {
+    try {
+      final response = await supabase.from('tbl_club').select();
+      setState(() {
+        Clublist = response;
+      });
+    } catch (e) {
+      print("ERROR FETCHING CLUB: $e");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fecthClub();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 233, 233, 233),
-                      borderRadius: BorderRadius.circular(5)),
-                  child: ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text(
-                        "Event 1",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text("DJ Auditorium"),
-                      trailing: const Icon(
-                        Icons.message,
-                        color: Colors.blue,
-                      )),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: Clublist.length,
+                    itemBuilder: (context, index) {
+                      final club = Clublist[index];
+
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: (Container(
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 241, 241, 241),
+                              borderRadius: BorderRadius.circular(10)),
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              /* Image.asset(event                        ), */
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(club['club_name'],
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              /* Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5.0),
+                                child: Text(club['event_venue'],
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis)),
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Text(club['event_details'],
+                                  maxLines: 2, overflow: TextOverflow.ellipsis),
+                            ) */
+                            ],
+                          ),
+                        )),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
